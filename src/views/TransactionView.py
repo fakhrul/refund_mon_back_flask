98c13d1aff98215e7a02f64c2dac7adf5e476d4a
Fakhrul Azran <fakhrulazran@gmail.com>
2024-07-17 16:06:06 +0800

    update db

--- src/views/TransactionView.py
+++ src/views/TransactionView.py
@@ -16,25 +16,25 @@ def get_all():
     FROM public."Refund_RefTrx" r
     LEFT JOIN public."Mst_ResponseCode" m ON r."ResponseCode" = m."ResponseCode"
     '''
-    result = db.engine.execute(query)
     transactions_list = []
-    for row in result:
-        transaction = {
-            "OrgCode": row["OrgCode"],
-            "HighwayCode": row["HighwayCode"],
-            "PlazaCode": row["PlazaCode"],
-            "SPID": row["SPID"],
-            "RefNo": row["RefNo"],
-            "Reason": row["Reason"],
-            "TransactionDateTime": row["TransactionDateTime"],
-            "ExitLocation": row["ExitLocation"],
-            "TransactionAmount": row["TransactionAmount"],
-            "ResponseCode": row["ResponseCode"],
-            "ResponseDesc": row["ResponseDesc"]
-        }
-        transactions_list.append(transaction)
+    with db.engine.connect() as connection:
+        result = connection.execute(query)
+        for row in result:
+            transaction = {
+                "OrgCode": row["OrgCode"],
+                "HighwayCode": row["HighwayCode"],
+                "PlazaCode": row["PlazaCode"],
+                "SPID": row["SPID"],
+                "RefNo": row["RefNo"],
+                "Reason": row["Reason"],
+                "TransactionDateTime": row["TransactionDateTime"],
+                "ExitLocation": row["ExitLocation"],
+                "TransactionAmount": row["TransactionAmount"],
+                "ResponseCode": row["ResponseCode"],
+                "ResponseDesc": row["ResponseDesc"]
+            }
+            transactions_list.append(transaction)
     return custom_response('success', '', transactions_list, 200)
-
 
 @transaction_api.route('/response_codes', methods=['GET'])
 def get_response_codes():
@@ -42,18 +42,17 @@ def get_response_codes():
     SELECT "ResponseCode", "ResponseDesc"
     FROM public."Mst_ResponseCode"
     '''
-    result = db.engine.execute(query)
     response_codes_list = []
-    for row in result:
-        response_code = {
-            "ResponseCode": row["ResponseCode"],
-            "ResponseDesc": row["ResponseDesc"]
-        }
-        response_codes_list.append(response_code)
-
+    with db.engine.connect() as connection:
+        result = connection.execute(query)
+        for row in result:
+            response_code = {
+                "ResponseCode": row["ResponseCode"],
+                "ResponseDesc": row["ResponseDesc"]
+            }
+            response_codes_list.append(response_code)
     return custom_response('success', '', response_codes_list, 200)
     
-
 def update_response_code(ref_no, response_code):
     update_query = '''
     UPDATE public."Refund_RefTrx"
@@ -68,23 +67,24 @@ def update_response_code(ref_no, response_code):
     WHERE r."RefNo" = %s
     '''
     try:
-        db.engine.execute(update_query, (response_code, ref_no))
-        result = db.engine.execute(select_query, (ref_no,))
-        updated_transaction = result.fetchone()
-        transaction = {
-            "OrgCode": updated_transaction["OrgCode"],
-            "HighwayCode": updated_transaction["HighwayCode"],
-            "PlazaCode": updated_transaction["PlazaCode"],
-            "SPID": updated_transaction["SPID"],
-            "RefNo": updated_transaction["RefNo"],
-            "Reason": updated_transaction["Reason"],
-            "TransactionDateTime": updated_transaction["TransactionDateTime"],
-            "ExitLocation": updated_transaction["ExitLocation"],
-            "TransactionAmount": updated_transaction["TransactionAmount"],
-            "ResponseCode": updated_transaction["ResponseCode"],
-            "ResponseCode": updated_transaction["ResponseCode"]
-        }
-        return {"status": "success", "transaction": transaction}
+        with db.engine.connect() as connection:
+            connection.execute(update_query, (response_code, ref_no))
+            result = connection.execute(select_query, (ref_no,))
+            updated_transaction = result.fetchone()
+            transaction = {
+                "OrgCode": updated_transaction["OrgCode"],
+                "HighwayCode": updated_transaction["HighwayCode"],
+                "PlazaCode": updated_transaction["PlazaCode"],
+                "SPID": updated_transaction["SPID"],
+                "RefNo": updated_transaction["RefNo"],
+                "Reason": updated_transaction["Reason"],
+                "TransactionDateTime": updated_transaction["TransactionDateTime"],
+                "ExitLocation": updated_transaction["ExitLocation"],
+                "TransactionAmount": updated_transaction["TransactionAmount"],
+                "ResponseCode": updated_transaction["ResponseCode"],
+                "ResponseDesc": updated_transaction["ResponseDesc"]
+            }
+            return {"status": "success", "transaction": transaction}
     except Exception as e:
         return {"status": "failure", "message": str(e)}
         
@@ -103,10 +103,9 @@ def custom_response(status, errorMsg, data, status_code):
     """
     Custom Response Function
     """
-
     info = {'status': status, 
             'errorMsg': errorMsg,
-            'data':data}
+            'data': data}
 
     response = Response(
         mimetype="application/json",
