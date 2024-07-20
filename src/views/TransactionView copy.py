b7af278578833027164a35c9c67d5b76f40d525c
Fakhrul Azran <fakhrulazran@gmail.com>
2024-07-20 16:27:03 +0800

    add transaction dtails

--- src/views/TransactionView copy.py
+++ src/views/TransactionView copy.py
@@ -6,7 +6,7 @@ from ..models import db
 app = Flask(__name__)
 transaction_api = Blueprint('transaction_api', __name__)
 from sqlalchemy import text
-import datetime
+
 
 @transaction_api.route('/', methods=['GET'])
 # @Auth.auth_required
@@ -71,8 +71,7 @@ def get_all():
                 "InvoiceDateTime": row[39],
                 "TngPaymentDateTime": row[40],
                 "ResponseCode": row[41],
-                "ResponseDesc": row[42],
-                # "Details": get_refund_ref_trx_details(row[4])
+                "ResponseDesc": row[42]
             }
             transactions_list.append(transaction)
     return custom_response('success', '', transactions_list, 200)
@@ -95,10 +94,10 @@ def get_response_codes():
     return custom_response('success', '', response_codes_list, 200)
 
 @transaction_api.route('/search', methods=['GET'])
-def get_transaction_by_refno_and_vehicleno():
-    ref_no = request.args.get('refNo', '')
-    vehicle_no = request.args.get('vehicleNo', '')
-
+def get_transaction_by_refno():
+    ref_no = request.args.get('refNo', 'UNKNOWN')
+    vehicle_no = request.args.get('vehicleNo', 'UNKNOWN')
+    print(ref_no, vehicle_no)
     query = text('''
     SELECT r."OrgCode", r."HighwayCode", r."PlazaCode", r."SPID", r."RefNo", r."Reason", r."VehicleNo", 
            r."RFIDTagNo", r."CardMfgno", r."VehicleColour", r."CarModel", r."CustomerName", r."CustomerSOFName]", 
@@ -165,141 +164,12 @@ def get_transaction_by_refno_and_vehicleno():
                     "InvoiceDateTime": row[39],
                     "TngPaymentDateTime": row[40],
                     "ResponseCode": row[41],
-                    "ResponseDesc": row[42],
-                    "Details": get_refund_ref_trx_details(row[4])
+                    "ResponseDesc": row[42]
                 }
                 transactions_list.append(transaction)
             return custom_response('success', '', transactions_list, 200)
         else:
             return custom_response('failure', 'Transactions not found', {}, 404)
-
-@transaction_api.route('/transaction/<ref_no>', methods=['GET'])
-def get_transaction_by_refno(ref_no):
-    query = text('''
-    SELECT r."OrgCode", r."HighwayCode", r."PlazaCode", r."SPID", r."RefNo", r."Reason", r."VehicleNo", 
-           r."RFIDTagNo", r."CardMfgno", r."VehicleColour", r."CarModel", r."CustomerName", r."CustomerSOFName]", 
-           r."EntryLocation", r."ExitLocation", r."EntrySPName", r."ExitSPName", r."TransactionID", 
-           r."TransactionDateTime", r."ActualFare", r."TransactionAmount", r."RefundAmount", r."TotalRefundAmount", 
-           r."PaymentDateTime", r."RefundStatus", r."BatchRefNo", r."Verify", r."RemarkDateTime", r."SPReason", 
-           r."SPReasonCode", r."AdditionalInfo", r."ActualEntryPlaza", r."ActualEntryLane", 
-           r."ActualEntryClassVehicle", r."ActualExitPlaza", r."ActualExitLane", r."ActualExitClassVehicle]", 
-           r."ReceivedDateTime", r."ResponseDateTime", r."InvoiceDateTime", r."TngPaymentDateTime", 
-           r."ResponseCode", m."ResponseDesc"
-    FROM public."Refund_RefTrx" r
-    LEFT JOIN public."Mst_ResponseCode" m ON r."ResponseCode" = m."ResponseCode"
-    WHERE r."RefNo" = :ref_no
-    ''')
-    
-    with db.engine.connect() as connection:
-        result = connection.execute(query, {'ref_no': ref_no})
-        transaction = result.fetchone()
-        if transaction:
-            transaction_data = {
-                "OrgCode": transaction[0],
-                "HighwayCode": transaction[1],
-                "PlazaCode": transaction[2],
-                "SPID": transaction[3],
-                "RefNo": transaction[4],
-                "Reason": transaction[5],
-                "VehicleNo": transaction[6],
-                "RFIDTagNo": transaction[7],
-                "CardMfgno": transaction[8],
-                "VehicleColour": transaction[9],
-                "CarModel": transaction[10],
-                "CustomerName": transaction[11],
-                "CustomerSOFName": transaction[12],
-                "EntryLocation": transaction[13],
-                "ExitLocation": transaction[14],
-                "EntrySPName": transaction[15],
-                "ExitSPName": transaction[16],
-                "TransactionID": transaction[17],
-                "TransactionDateTime": transaction[18],
-                "ActualFare": transaction[19],
-                "TransactionAmount": transaction[20],
-                "RefundAmount": transaction[21],
-                "TotalRefundAmount": transaction[22],
-                "PaymentDateTime": transaction[23],
-                "RefundStatus": transaction[24],
-                "BatchRefNo": transaction[25],
-                "Verify": transaction[26],
-                "RemarkDateTime": transaction[27],
-                "SPReason": transaction[28],
-                "SPReasonCode": transaction[29],
-                "AdditionalInfo": transaction[30],
-                "ActualEntryPlaza": transaction[31],
-                "ActualEntryLane": transaction[32],
-                "ActualEntryClassVehicle": transaction[33],
-                "ActualExitPlaza": transaction[34],
-                "ActualExitLane": transaction[35],
-                "ActualExitClassVehicle": transaction[36],
-                "ReceivedDateTime": transaction[37],
-                "ResponseDateTime": transaction[38],
-                "InvoiceDateTime": transaction[39],
-                "TngPaymentDateTime": transaction[40],
-                "ResponseCode": transaction[41],
-                "ResponseDesc": transaction[42],
-                "Details": get_refund_ref_trx_details(transaction[4])
-            }
-            return custom_response('success', '', transaction_data, 200)
-        else:
-            return custom_response('failure', 'Transaction not found', {}, 404)
-        
-# def get_refund_ref_trx_details(ref_no):
-#     query = text('''
-#     SELECT "OrgCode", "HighwayCode", "PlazaCode", "SPID", "RefNo", "SeqNo", "UpdateBy", 
-#            "CodeStatus", "Reason", "ResponseDateTime"
-#     FROM public."Refund_RefTrxDetails"
-#     WHERE "RefNo" = :ref_no
-#     ''')
-    
-#     with db.engine.connect() as connection:
-#         result = connection.execute(query, {'ref_no': ref_no})
-#         details_list = []
-#         for row in result:
-#             detail = {
-#                 "OrgCode": row[0],
-#                 "HighwayCode": row[1],
-#                 "PlazaCode": row[2],
-#                 "SPID": row[3],
-#                 "RefNo": row[4],
-#                 "SeqNo": row[5],
-#                 "UpdateBy": row[6],
-#                 "CodeStatus": row[7],
-#                 "Reason": row[8],
-#                 "ResponseDateTime": row[9]
-#             }
-#             details_list.append(detail)
-#     return details_list
-
-def get_refund_ref_trx_details(ref_no):
-    query = text('''
-    SELECT d."OrgCode", d."HighwayCode", d."PlazaCode", d."SPID", d."RefNo", d."SeqNo", d."UpdateBy", 
-           d."CodeStatus", d."Reason", d."ResponseDateTime", r."ResponseDesc"
-    FROM public."Refund_RefTrxDetails" d
-    LEFT JOIN public."Mst_ResponseCode" r ON d."CodeStatus" = r."ResponseCode"
-    WHERE d."RefNo" = :ref_no
-    ORDER BY d."ResponseDateTime"
-    ''')
-    
-    with db.engine.connect() as connection:
-        result = connection.execute(query, {'ref_no': ref_no})
-        details_list = []
-        for row in result:
-            detail = {
-                "OrgCode": row[0],
-                "HighwayCode": row[1],
-                "PlazaCode": row[2],
-                "SPID": row[3],
-                "RefNo": row[4],
-                "SeqNo": row[5],
-                "UpdateBy": row[6],
-                "CodeStatus": row[7],
-                "Reason": row[8],
-                "ResponseDateTime": row[9],
-                "ResponseDesc": row[10]
-            }
-            details_list.append(detail)
-    return details_list
 
 def update_response_code(ref_no, response_code):
     update_query = text('''
@@ -316,20 +186,10 @@ def update_response_code(ref_no, response_code):
            r."SPReasonCode", r."AdditionalInfo", r."ActualEntryPlaza", r."ActualEntryLane", 
            r."ActualEntryClassVehicle", r."ActualExitPlaza", r."ActualExitLane", r."ActualExitClassVehicle]", 
            r."ReceivedDateTime", r."ResponseDateTime", r."InvoiceDateTime", r."TngPaymentDateTime", 
-           r."ResponseCode", m."ResponseDesc", array_agg(d.*) as "Details"
+           r."ResponseCode", m."ResponseDesc"
     FROM public."Refund_RefTrx" r
     LEFT JOIN public."Mst_ResponseCode" m ON r."ResponseCode" = m."ResponseCode"
-    LEFT JOIN public."Refund_RefTrxDetails" d ON r."RefNo" = d."RefNo"
     WHERE r."RefNo" = :ref_no
-    GROUP BY r."OrgCode", r."HighwayCode", r."PlazaCode", r."SPID", r."RefNo", r."Reason", r."VehicleNo", 
-             r."RFIDTagNo", r."CardMfgno", r."VehicleColour", r."CarModel", r."CustomerName", r."CustomerSOFName]", 
-             r."EntryLocation", r."ExitLocation", r."EntrySPName", r."ExitSPName", r."TransactionID", 
-             r."TransactionDateTime", r."ActualFare", r."TransactionAmount", r."RefundAmount", r."TotalRefundAmount", 
-             r."PaymentDateTime", r."RefundStatus", r."BatchRefNo", r."Verify", r."RemarkDateTime", r."SPReason", 
-             r."SPReasonCode", r."AdditionalInfo", r."ActualEntryPlaza", r."ActualEntryLane", 
-             r."ActualEntryClassVehicle", r."ActualExitPlaza", r."ActualExitLane", r."ActualExitClassVehicle]", 
-             r."ReceivedDateTime", r."ResponseDateTime", r."InvoiceDateTime", r."TngPaymentDateTime", 
-             r."ResponseCode", m."ResponseDesc"
     ''')
     try:
         with db.engine.connect() as connection:
@@ -379,8 +239,7 @@ def update_response_code(ref_no, response_code):
                 "InvoiceDateTime": updated_transaction[39],
                 "TngPaymentDateTime": updated_transaction[40],
                 "ResponseCode": updated_transaction[41],
-                "ResponseDesc": updated_transaction[42],
-                "Details": get_refund_ref_trx_details(ref_no)
+                "ResponseDesc": updated_transaction[42]
             }
             return {"status": "success", "transaction": transaction}
     except Exception as e:
@@ -396,43 +255,6 @@ def update_response():
         return custom_response('success', '', result['transaction'], 200)
     else:
         return custom_response('failure', result['message'], {}, 500)
-
-def get_next_seqno(ref_no):
-    query = text('''
-    SELECT MAX(CAST("SeqNo" AS INTEGER))
-    FROM public."Refund_RefTrxDetails"
-    WHERE "RefNo" = :ref_no
-    ''')
-    
-    with db.engine.connect() as connection:
-        result = connection.execute(query, {'ref_no': ref_no}).scalar()
-        if result is None:
-            return 1
-        else:
-            return result + 1
-        
-@transaction_api.route('/add_detail', methods=['POST'])
-def add_refund_ref_trx_detail():
-    data = request.get_json()
-    print(data)
-    data['SeqNo'] = str(get_next_seqno(data['RefNo']))
-    data['UpdateBy'] = 0
-    data['ResponseDateTime'] =  datetime.datetime.utcnow()
-    
-    insert_query = text('''
-    INSERT INTO public."Refund_RefTrxDetails" ("OrgCode", "HighwayCode", "PlazaCode", "SPID", "RefNo", "SeqNo", 
-                                               "UpdateBy", "CodeStatus", "Reason", "ResponseDateTime")
-    VALUES (:OrgCode, :HighwayCode, :PlazaCode, :SPID, :RefNo, :SeqNo, :UpdateBy, :CodeStatus, :Reason, :ResponseDateTime)
-    ''')
-    # try:
-    #     with db.engine.connect() as connection:
-    #         connection.execute(insert_query, data)
-    #     return custom_response('success', 'Detail added successfully', {}, 201)
-    # except Exception as e:
-    #     return custom_response('failure', str(e), {}, 500)
-    with db.engine.connect() as connection:
-        connection.execute(insert_query, data)
-    return custom_response('success', 'Detail added successfully', {}, 201)
 
 def custom_response(status, errorMsg, data, status_code):
     """
